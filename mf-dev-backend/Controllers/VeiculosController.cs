577b2fa199601057a70fc18a46887b8d648a79f0
cassianobaptista <cassianotbap@gmail.com>
2024-04-24 14:19:07 -0300

    Inclusão dos Relatórios

--- mf-dev-backend/Controllers/VeiculosController.cs
+++ mf-dev-backend/Controllers/VeiculosController.cs
@@ -110,5 +110,25 @@ namespace mf_dev_backend.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Relatorio(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+                return NotFound();
+
+            var consumos = await _context.Consumos.Where(c => c.VeiculoId == id).OrderByDescending(c => c.data).ToListAsync();
+
+            decimal total = consumos.Sum(c => c.valor);
+
+            ViewBag.Veiculo = veiculo; 
+            ViewBag.Total = total;
+
+            return View(consumos);
+        }
     }
 }
